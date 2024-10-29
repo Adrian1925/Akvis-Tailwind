2f6eba240a66f5a5701abe0f3f45e80d1ed0efd3
Adrian1925 <adrianyoga573@gmail.com>
2024-10-29 21:05:49 +0700

    change number wa

--- js/beli.js
+++ js/beli.js
@@ -2,7 +2,7 @@ function sendwa(event) {
     event.preventDefault();
 
     const message = "Halo, saya ingin memesan desain 3D.";
-    const phoneNumber = "6281806067144"; // Ganti dengan nomor WhatsApp yang diinginkan
+    const phoneNumber = "6285158883609"; // Ganti dengan nomor WhatsApp yang diinginkan
     const urlTowhatsapp = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
 
     // Menggunakan lokasi.href untuk mengarahkan pengguna
